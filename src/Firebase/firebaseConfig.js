9391c35c682f0f84c0307725d1ad42bce8af2550
mstsurnalyakter <surnalyms@gmail.com>
2024-04-11 13:43:08 +0600

    firebase setup

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -2,17 +2,16 @@
  import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpdyTkl9jpS_2GE1JHDJB_xhhaxHVK8jE",
-  authDomain: "glasses-authentication.firebaseapp.com",
-  projectId: "glasses-authentication",
-  storageBucket: "glasses-authentication.appspot.com",
-  messagingSenderId: "233349205700",
-  appId: "1:233349205700:web:5d00f4eff4684c90c38391"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 export default auth;
